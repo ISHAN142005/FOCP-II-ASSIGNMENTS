e20077a762f09e1af008c146da9b5b77d56cfba8
ISHAN142005 <ishanbohra1414@gmail.com>
2026-02-10 22:53:55 +0530

    Q22

---
+++ LAB MANUAL/Q22.cpp
@@ -0,0 +1,64 @@
+/*
+Q22. A jewelry design tool generates hollow diamond outlines for patterns. Implement a solution to display a
+hollow diamond pattern of *.
+    *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows for the upper half: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j == 1 || j == 2 * i - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j == 1 || j == 2 * i - 1)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
