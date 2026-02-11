bcd8865ba5266854b1f497ba6af851236482c1b4
ISHAN142005 <ishanbohra1414@gmail.com>
2026-02-11 22:12:53 +0530

    Q24

---
+++ LAB MANUAL/Q24.cpp
@@ -0,0 +1,29 @@
+/*
+A typing practice app displays the alphabets in pyramid form.
+Implement a solution to display:
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (char ch = 'A'; ch < 'A' + i; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
